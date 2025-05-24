0d9e07550127dc0f20e9240afd656aa290051a9d
vitaly <vitalicheg28@mail.ru>
2025-05-24 16:47:11 +0200

    feat: add route db checker

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	//"github.com/sirupsen/logrus"
 )
+
+var db *sql.DB
 
 func main() {
 	// Создаем новый экземпляр Gin
@@ -23,6 +26,16 @@ func main() {
 		c.String(http.StatusOK, "Hello, world!")
 	})
 
+	var err error
+    // Подключение к БД (пример)
+    connStr := "postgresql://user:password@hotels-db:26257/hotels?sslmode=disable"
+    db, err = sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatalf("Cannot connect to DB: %v", err)
+    }
+
+	http.HandleFunc("/health/db", dbHealthCheckHandler)
+
 	//logrus.Error("ests")
 	//logrus.Debug("sfds")
 	//logrus.Debug("Hotel service started")
@@ -31,4 +44,13 @@ func main() {
 	if err := r.Run(":9064"); err != nil {
 		log.Fatal("Error starting server: ", err)
 	}
+}
+
+func dbHealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+    err := db.Ping()
+    if err != nil {
+        http.Error(w, "Database connection failed", http.StatusServiceUnavailable)
+        return
+    }
+    fmt.Fprintln(w, "Database connection OK")
 }
